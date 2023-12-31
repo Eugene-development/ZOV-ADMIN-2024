1f11c28b2563ced940ad36a8e308ee68875543fc
Eugene Che <indexpro24@gmail.com>
2023-12-31 17:36:56 +0300

    с

--- project/src/app/(app)/layout.js
+++ project/src/app/(app)/layout.js
@@ -14,13 +14,13 @@ const AppLayout = ({ children, header }) => {
 
     return (
         <div className="min-h-screen bg-gray-100">
-            <Navigation user={user} />
+            {/* <Navigation user={user} /> */}
 
-            <header className="bg-white shadow">
+            {/* <header className="bg-white shadow">
                 <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     {header}
                 </div>
-            </header>
+            </header> */}
 
             <main>
                 <Header />
