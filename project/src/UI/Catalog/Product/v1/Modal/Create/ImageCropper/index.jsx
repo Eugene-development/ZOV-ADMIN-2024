d4e244e0392d838e3b5ef2480ec0eb70f2812feb
Eugene Che <indexpro24@gmail.com>
2024-01-02 17:14:56 +0300

    c

--- project/src/UI/Catalog/Product/v1/Modal/Create/ImageCropper/index.jsx
+++ project/src/UI/Catalog/Product/v1/Modal/Create/ImageCropper/index.jsx
@@ -5,11 +5,66 @@ import { CheckIcon } from '@heroicons/react/24/outline'
 import { useProductStore } from '@/store/product'
 const { visibleCreateImageProductModal } = useProductStore
 
+
+
+import Cropper from "react-cropper";
+import "cropperjs/dist/cropper.css";
+import { useImage } from '@/lib/image'
+
+const defaultSrc =
+  "https://raw.githubusercontent.com/roadmanfong/react-cropper/master/example/img/child.jpg";
+
+
 export default () => {
 
   const { currentVisibleCreateImageProductModal, closeVisibleCreateImageProductModal } = visibleCreateImageProductModal()
 
   const cancelButtonRef = useRef(null)
+
+  const { sendImageToBucket } = useImage();
+
+
+
+
+    const [image, setImage] = useState();
+    const [cropper, setCropper] = useState();
+    const [cropData, setCropData] = useState("#");
+
+    const handleFileChange = (e) => {
+        e.preventDefault();
+        let files;
+        if (e.dataTransfer) {
+            files = e.dataTransfer.files;
+        } else if (e.target) {
+            files = e.target.files;
+        }
+        const reader = new FileReader();
+        reader.onload = () => {
+            setImage(reader.result);
+        };
+        reader.readAsDataURL(files[0]);
+    };
+
+    const getCropData = () => {
+        if (typeof cropper !== "undefined") {
+        setCropData(cropper.getCroppedCanvas().toDataURL());
+        }
+    };
+
+
+
+
+    const handleAddProduct = () => {
+        try {
+                cropper.getCroppedCanvas().toBlob(async (cropData) => {
+                    const hashNameImage = await sendImageToBucket(cropData);
+                    // Записать в стор 1 хэш (пока один), кроппер с одинаковым соотношением сторон, Закрыть окно добавления изображения
+                    console.log(hashNameImage)
+                }, 'image/*');
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
   return (
     <Transition.Root show={currentVisibleCreateImageProductModal} as={Fragment}>
@@ -42,28 +97,62 @@ export default () => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
-                <div>
-                  <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
-                    <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                  </div>
-                  <div className="mt-3 text-center sm:mt-5">
-                    <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                      Payment successful
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius aliquam laudantium explicabo
-                        pariatur iste dolorem animi vitae error totam. At sapiente aliquam accusamus facere veritatis.
-                      </p>
+    <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl sm:p-6">
+      <div className="mt-8" style={{ width: "100%" }}>
+        <input type="file" className="block w-full text-sm text-slate-500
+            file:mr-4 file:py-2 file:px-4
+            file:rounded-full file:border-0
+            file:text-sm file:font-semibold
+            file:bg-green-50 file:text-green-700
+            hover:file:bg-violet-100
+            " onChange={handleFileChange} />
+        <br />
+        <br />
+        {image &&
+            <Cropper
+            style={{ height: 400, width: "100%" }}
+            zoomTo={0.5}
+            initialAspectRatio={1}
+            src={image}
+            viewMode={1}
+            minCropBoxHeight={10}
+            minCropBoxWidth={10}
+            background={false}
+            responsive={true}
+            autoCropArea={1}
+            checkOrientation={false} // https://github.com/fengyuanchen/cropperjs/issues/671
+            onInitialized={(instance) => {
+                setCropper(instance);
+            }}
+            guides={true}
+            />
+        }
+      </div>
+
+            {image &&
+                <div className="flex pt-4 justify-between">
+                    <div className="">
+                        <button type="button" onClick={getCropData} className=" inline-flex items-center rounded-md border border-transparent bg-green-100 px-6 py-3 text-base font-medium text-green-700 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2">Обрезать</button>
                     </div>
-                  </div>
+                    {cropData !== "#" &&
+                        <>
+
+                            <div
+
+                            style={{ width: "50%", float: "center" }}
+                            >
+                            {/* <span className="mb-2">Результат:</span> */}
+                            <img style={{ width: "100%" }} src={cropData} />
+                            </div>
+                        </>
+                    }
                 </div>
+            }
                 <div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                   <button
                     type="button"
                     className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2"
-                    onClick={() => closeVisibleCreateImageProductModal()}
+                    onClick={() => handleAddProduct()}
                   >
                     Deactivate
                   </button>
