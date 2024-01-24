479988fc1c62a9033ab2dc68b52c609cd6dad39e
Eugene Che <indexpro24@gmail.com>
2024-01-24 07:58:00 +0300

    c

--- project/src/app/LoginLinks.js
+++ project/src/app/LoginLinks.js
@@ -7,21 +7,24 @@ const LoginLinks = () => {
     const { user } = useAuth({ middleware: 'guest' })
 
     return (
-        <div className="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        <div className="fixed top-0 right-0 px-6 py-4 sm:block">
             {user ? (
-                <Link href="/dashboard" className="ml-4 text-sm text-gray-100">
+                <Link
+                    href="/dashboard"
+                    className="ml-4 text-sm text-gray-100 bg-gray-700 px-3 py-1 rounded-xl">
                     Главная
                 </Link>
             ) : (
                 <>
-                    <Link href="/login" className="text-sm text-gray-100">
+                    <Link
+                        href="/login"
+                        className="text-sm text-gray-100 bg-gray-700 px-3 py-1 rounded-xl">
                         Вход
                     </Link>
 
                     <Link
                         href="/register"
-                        className="ml-4 text-sm text-gray-100"
-                    >
+                        className="ml-4 text-sm text-gray-100 bg-gray-700 px-3 py-1 rounded-xl">
                         Регистрация
                     </Link>
                 </>
